78bc59eddae790d86b0b14f47e9215b21ffaab47
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-09 06:13:05 -0300

    refactor:  change on app directories structure

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 use App\Service\Router;
 
-require_once __DIR__ . '/../app/service/router/Router.php';
-require_once __DIR__ . '/../app/service/router/routes.php';
+require_once __DIR__ . '/../app/router/Router.php';
+require_once __DIR__ . '/../app/router/routes.php';
 
 Router::route();
