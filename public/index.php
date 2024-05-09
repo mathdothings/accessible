426630796c39c7122bbc1ce904a53bdee33ef536
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-08 22:47:48 -0300

    wip: routing handling

--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
 
-var_dump($_SERVER);
+use App\Service\Router;
+
+require_once __DIR__ . '/../app/service/Router.php';
+
+Router::add(method: 'get', route: '/', callback: function () {
+    echo 'Home Page';
+});
+
+Router::add(method: 'get', route: '/login', callback: function () {
+    echo 'Login Page';
+});
+
+Router::add(method: 'get', route: '/signup', callback: function () {
+    echo 'Signup Page';
+});
+
+Router::add(method: 'post', route: '/login', callback: function () {
+    echo 'Signup Page';
+});
+
+Router::route();
