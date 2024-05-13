74e27b4ba12c84c0c84d262ed278cd6a29c91012
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-13 06:21:48 -0300

    feat: require dd function

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use App\Service\Router;
 
+require_once __DIR__ . '/../app/helper/functions.php';
 require_once __DIR__ . '/../app/router/Router.php';
 require_once __DIR__ . '/../app/router/routes.php';
 
