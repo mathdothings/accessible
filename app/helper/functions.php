83c625910519a7c758e0f4f637e367f60a8f2c33
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-21 17:06:57 -0300

    feat: returns an array of all styles relative paths

--- app/helper/functions.php
+++ app/helper/functions.php
@@ -5,3 +5,29 @@ function dd($value): void
     var_dump($value);
     die;
 }
+
+function listStyles(string $directory): array
+{
+    $styles = [];
+
+    function findFiles(string $directory, array &$styles)
+    {
+        $files = scandir($directory);
+        foreach ($files as $file) {
+            if ($file != "." && $file != "..") {
+                $currentDirectory = $directory . '/' . $file;
+                if (is_dir($currentDirectory)) {
+                    findFiles($currentDirectory, $styles);
+                } else {
+                    $styles[] = str_replace(STYLE_ROOT, '', $currentDirectory);
+                }
+            }
+        }
+
+        return $styles;
+    }
+
+    $styles = findFiles($directory, $styles);
+
+    return $styles;
+}
