8b03c5a43fdcc115774e1c3fe4acff404aa6d3b5
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-21 17:08:09 -0300

    refactor: improve styles loading by dinamically loadling all styles in /public/style/ directory

--- app/view/partial/head.php
+++ app/view/partial/head.php
@@ -5,8 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accessible</title>
-    <!-- <link rel="stylesheet" href="/public/style/style.css"> -->
-    <link rel="stylesheet" href="<?= '/public/style/style.css?v=' . time(); ?>">
+    <?php
+    $styles = listStyles(STYLE_ROOT);
+    foreach ($styles as $style) { ?>
+        <link rel="stylesheet" href="<?= '/public/style/' . $style . '?v=' . time(); ?>">
+    <?php } ?>
 </head>
 
 <body>
