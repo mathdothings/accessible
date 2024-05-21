3810af6b60d28884c0adcda3564cfe1846723141
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-21 17:05:52 -0300

    feat: new const for app styles directory root

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use App\Router\Router;
 
 const ROOT = __DIR__ . '/../';
+const STYLE_ROOT = __DIR__ . '/style/';
 
 require_once ROOT . '/app/helper/functions.php';
 require_once ROOT . '/app/router/Router.php';
