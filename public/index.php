844c377691e63e246562ffa3185c26ad440d185a
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-30 06:14:28 -0300

    refactor: code readability

--- public/index.php
+++ public/index.php
@@ -5,9 +5,21 @@ use App\Router\Router;
 const ROOT = __DIR__ . '/../';
 const STYLE_ROOT = __DIR__ . '/style/';
 
+// helper
 require_once ROOT . '/app/helper/functions.php';
+
+// router
 require_once ROOT . '/app/router/Router.php';
 require_once ROOT . '/app/router/routes.php';
+
+// view
+require_once ROOT . 'app/view/View.php';
+
+// service
+require_once ROOT . 'app/service/Authentication.php';
+
+// controller
+require_once ROOT . 'app/controller/login/LoginController.php';
 
 Router::route();
 
