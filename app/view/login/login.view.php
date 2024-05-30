d3ad33c35ff6052424787ee5f3a11c803bd47bdd
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-30 06:10:56 -0300

    feat: login page

--- app/view/login/login.view.php
+++ app/view/login/login.view.php
@@ -1,8 +1,19 @@
 <?php require_once ROOT . '/app/view/partial/head.php'; ?>
 
-<main>
-    <h1>Accessible</h1>
-    <h2>Log in!</h2>
-</main>
-
+<article class="center-container">
+    <main class="main container">
+        <form method="post" class="flex-container main__form__card main__form" autocomplete="off">
+            <h1 class="main__form__title">Accessible</h1>
+            <label for="email"></label>
+            <input type="text" name="email" id="login-email" autofocus placeholder="Email" class="main__form__input" value="">
+            <smail class="main__form__validation main__form__validation-error"></smail>
+            <label for="password"></label>
+            <input type="password" name="password" id="login-password" placeholder="Password" class="main__form__input">
+            <smail class="main__form__validation main__form__validation-error"></smail>
+            <input type="submit" name="login" class="main__form__button" value="Log in!">
+            <small class="main__form__validation main__form__validation-success"></small>
+        </form>
+        <p class="main__form__paragraph main__form__card">Don't have an account? <a href="/signup" class="main__form__paragraph__link">Sign up!</a></p>
+    </main>
+</article>
 <?php require_once ROOT . '/app/view/partial/foot.php'; ?>
