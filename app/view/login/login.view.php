3dfa8016300f4ac810e44b0e679efbe6a0efa36a
mathdothings <jhonata.prodrigues@gmail.com>
2024-05-31 04:51:59 -0300

    refactor: login page styles now has properly his own stylesheet

--- app/view/login/login.view.php
+++ app/view/login/login.view.php
@@ -2,18 +2,18 @@
 
 <article class="center-container">
     <main class="main container">
-        <form method="post" class="flex-container main__form__card main__form" autocomplete="off">
-            <h1 class="main__form__title">Accessible</h1>
+        <form method="post" class="flex-container login-main__form__card login-main__form" autocomplete="off">
+            <h1 class="login-main__form__title">Accessible</h1>
             <label for="email"></label>
-            <input type="text" name="email" id="login-email" autofocus placeholder="Email" class="main__form__input" value="">
-            <smail class="main__form__validation main__form__validation-error"></smail>
+            <input type="text" name="email" id="login-email" autofocus placeholder="Email" class="login-main__form__input" value="">
+            <smail class="login-main__form__validation login-main__form__validation-error"></smail>
             <label for="password"></label>
-            <input type="password" name="password" id="login-password" placeholder="Password" class="main__form__input">
-            <smail class="main__form__validation main__form__validation-error"></smail>
-            <input type="submit" name="login" class="main__form__button" value="Log in!">
-            <small class="main__form__validation main__form__validation-success"></small>
+            <input type="password" name="password" id="login-password" placeholder="Password" class="login-main__form__input">
+            <smail class="login-main__form__validation login-main__form__validation-error"></smail>
+            <input type="submit" name="login" class="login-main__form__button" value="Log in!">
+            <small class="login-main__form__validation login-main__form__validation-success"></small>
         </form>
-        <p class="main__form__paragraph main__form__card">Don't have an account? <a href="/signup" class="main__form__paragraph__link">Sign up!</a></p>
+        <p class="login-main__form__paragraph login-main__form__card">Don't have an account? <a href="/signup" class="login-main__form__paragraph__link">Sign up!</a></p>
     </main>
 </article>
 <?php require_once ROOT . '/app/view/partial/foot.php'; ?>
