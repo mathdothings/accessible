5b29bac65403da31f1cfabc83fe5e7217a5d8851
mathdothings <jhonata.prodrigues@gmail.com>
2024-06-01 18:50:59 -0300

    refactor: let composer use psr-4 autoload

--- public/index.php
+++ public/index.php
@@ -1,27 +1,13 @@
 <?php
 
-use App\Router\Router;
-
 const ROOT = __DIR__ . '/../';
 const STYLE_ROOT = __DIR__ . '/style/';
 
-// helper
+require ROOT . 'vendor/autoload.php';
 require_once ROOT . '/app/helper/functions.php';
+require_once ROOT . '/app/Router/routes.php';
 
-// router
-require_once ROOT . '/app/router/Router.php';
-require_once ROOT . '/app/router/routes.php';
-
-// view
-require_once ROOT . '/app/enum/ViewsPath.php';
-require_once ROOT . '/app/view/View.php';
-
-// service
-require_once ROOT . '/app/service/Authentication.php';
-
-// controller
-require_once ROOT . '/app/controller/login/LoginController.php';
+use App\Router\Router;
 
 Router::route();
-
 session_start();
