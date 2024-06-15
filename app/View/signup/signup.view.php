a4afce9b1bebf314c1a203e306fb9a5c736effbc
mathdothings <jhonata.prodrigues@gmail.com>
2024-06-15 14:59:04 -0300

    feat: show validation errors

--- app/View/signup/signup.view.php
+++ app/View/signup/signup.view.php
@@ -5,13 +5,39 @@
         <form method="post" class="flex-container signup-main__form__card signup-main__form" autocomplete="off">
             <h1 class="signup-main__form__title">Accessible</h1>
             <label for="signup-name"></label>
-            <input type="text" name="signup-name" id="signup-name" autofocus placeholder="Name" class="signup-main__form__input" value="">
+            <input type="text" name="signup-name" id="signup-name" autofocus placeholder="Name" class="signup-main__form__input" value="<?= htmlspecialchars($_POST['signup-name'] ?? '') ?>">
+            <ul class="signup-main__form__validation">
+                <?php if (isset($errors)) {
+                    foreach ($errors['name'] as $value) { ?>
+                        <li class="signup-main__form__validation-error "><small><?= $value ?></small></li>
+                <?php }
+                } ?>
+            </ul>
             <label for="signup-email"></label>
-            <input type="text" name="signup-email" id="signup-email" placeholder="Email" class="signup-main__form__input" value="">
+            <input type="text" name="signup-email" id="signup-email" placeholder="Email" class="signup-main__form__input" value="<?= htmlspecialchars($_POST['signup-email']?? '') ?>">
+            <ul class="signup-main__form__validation">
+                <?php if (isset($errors)) {
+                    foreach ($errors['email'] as $value) { ?>
+                        <li class="signup-main__form__validation-error "><small><?= $value ?></small></li>
+                <?php }
+                } ?>
+            </ul>
             <label for="signup-password"></label>
             <input type="password" name="signup-password" id="signup-password" placeholder="Password" class="signup-main__form__input">
+            <ul class="signup-main__form__validation">
+                <?php if (isset($errors)) {
+                    foreach ($errors['password'] as $value) { ?>
+                        <li class="signup-main__form__validation-error "><small><?= $value ?></small></li>
+                <?php }
+                } ?>
+            </ul>
             <label for="signup-repeat-password"></label>
             <input type="password" name="signup-repeat-password" id="signup-repeat-password" placeholder="Repeat Password" class="signup-main__form__input">
+            <ul class="signup-main__form__validation">
+                <?php if (isset($errors['repeatPassword'])) { ?>
+                    <li class="signup-main__form__validation-error "><small><?= $errors['repeatPassword'] ?></small></li>
+                <?php } ?>
+            </ul>
             <input type="submit" name="signup" class="signup-main__form__button" value="Sign up!">
             <small class="signup-main__form__validation signup-main__form__validation-success"></small>
             <small class="signup-main__form__validation signup-main__form__validation-error signup-main__form__validation-error--highlight"></small>
