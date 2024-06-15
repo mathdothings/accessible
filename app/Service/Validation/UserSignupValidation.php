a2918a20fb7d077b3b9338da56240147598dbcf1
mathdothings <jhonata.prodrigues@gmail.com>
2024-06-15 14:57:52 -0300

    refactor: improve readability method names

--- app/Service/Validation/UserSignupValidation.php
+++ app/Service/Validation/UserSignupValidation.php
@@ -8,13 +8,14 @@ use App\Trait\UserValidation;
 class UserSignupValidation implements UserSignupValidationInterface
 {
     use UserValidation;
-    static public function validate(UserSignupDTO $userSignupDTO): bool | array
+
+    static public function isValid(UserSignupDTO $userSignupDTO): bool | array
     {
         self::isValidName($userSignupDTO->Name);
         self::isValidEmail($userSignupDTO->Email);
         self::isValidPassword($userSignupDTO->Password);
         self::isValidRepeatPassword($userSignupDTO->Password, $userSignupDTO->RepeatPassword);
 
-        return self::isValid();
+        return self::validate();
     }
 }
