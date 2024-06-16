6fd3c81f1140e1cab004317fe63b38f1eb007c15
mathdothings <jhonata.prodrigues@gmail.com>
2024-06-16 14:16:18 -0300

    feat: repository pattern interface

---
+++ app/Service/Repository/UserRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service\Repository;
+
+use App\DTO\UserSignupDTO;
+use App\Model\UserModel;
+
+interface UserRepositoryInterface
+{
+    function save(UserSignupDTO $userSignupDTO): bool;
+    function findById(int $id): UserModel;
+    function findAll(): array;
+    function change(UserModel $user): bool;
+    function remove(UserSignupDTO $user): bool;
+}
