384c869228005e49e2eda3e22514b3ffb1b80cba
mathdothings <jhonata.prodrigues@gmail.com>
2024-07-03 09:08:23 -0300

    refactor: improve readability for error checking

--- app/View/signup/signup.view.php
+++ app/View/signup/signup.view.php
@@ -1,5 +1,13 @@
 <?php require_once ROOT . '/app/View/partial/head.php'; ?>
 
+<?php
+$validationErros = null;
+if (!empty($errors['validation']) && is_array($errors['validation'])) $validationErros = $errors['validation'];
+
+$success = null;
+if (!empty($errors['persistance'])) $success = $errors['persistance'];
+?>
+
 <article class="center-container">
     <main class="main container">
         <form method="post" class="flex-container signup-main__form__card signup-main__form" autocomplete="off">
@@ -7,40 +15,44 @@
             <label for="signup-name"></label>
             <input type="text" name="signup-name" id="signup-name" autofocus placeholder="Name" class="signup-main__form__input" value="<?= htmlspecialchars($_POST['signup-name'] ?? '') ?>">
             <ul class="signup-main__form__validation">
-                <?php if (isset($errors)) {
-                    foreach ($errors['name'] as $value) { ?>
+                <?php if ($validationErros) : ?>
+                    <?php foreach ($validationErros['name'] as $value) : ?>
                         <li class="signup-main__form__validation-error "><small><?= $value ?></small></li>
-                <?php }
-                } ?>
+                    <?php endforeach ?>
+                <?php endif ?>
             </ul>
             <label for="signup-email"></label>
             <input type="text" name="signup-email" id="signup-email" placeholder="Email" class="signup-main__form__input" value="<?= htmlspecialchars($_POST['signup-email'] ?? '') ?>">
             <ul class="signup-main__form__validation">
-                <?php if (isset($errors)) {
-                    foreach ($errors['email'] as $value) { ?>
+                <?php if ($validationErros) : ?>
+                    <?php foreach ($validationErros['email'] as $value) : ?>
                         <li class="signup-main__form__validation-error "><small><?= $value ?></small></li>
-                <?php }
-                } ?>
+                    <?php endforeach ?>
+                <?php endif ?>
             </ul>
             <label for="signup-password"></label>
             <input type="password" name="signup-password" id="signup-password" placeholder="Password" class="signup-main__form__input">
             <ul class="signup-main__form__validation">
-                <?php if (isset($errors)) {
-                    foreach ($errors['password'] as $value) { ?>
+                <?php if ($validationErros) : ?>
+                    <?php foreach ($validationErros['password'] as $value) : ?>
                         <li class="signup-main__form__validation-error "><small><?= $value ?></small></li>
-                <?php }
-                } ?>
+                    <?php endforeach ?>
+                <?php endif ?>
             </ul>
             <label for="signup-repeat-password"></label>
             <input type="password" name="signup-repeat-password" id="signup-repeat-password" placeholder="Repeat Password" class="signup-main__form__input">
             <ul class="signup-main__form__validation">
-                <?php if (!empty($errors['repeatPassword'])) { ?>
-                    <li class="signup-main__form__validation-error "><small><?= $errors['repeatPassword'] ?></small></li>
-                <?php } ?>
+                <?php if (!empty($validationErros['repeatPassword'])) : ?>
+                    <li class="signup-main__form__validation-error "><small><?= $validationErros['repeatPassword'] ?></small></li>
+                <?php endif ?>
             </ul>
             <input type="submit" name="signup" class="signup-main__form__button" value="Sign up!">
-            <small class="signup-main__form__validation signup-main__form__validation-success"></small>
-            <small class="signup-main__form__validation signup-main__form__validation-error signup-main__form__validation-error--highlight"></small>
+            <?php if ($success) : ?>
+                <small class="signup-main__form__validation signup-main__form__validation-success"><?= 'Success!' ?></small>
+            <?php endif ?>
+            <?php if (!empty($success) && !$success) : ?>
+                <small class="signup-main__form__validation signup-main__form__validation-error signup-main__form__validation-error--highlight"><?= 'Error!' ?></small>
+            <?php endif ?>
         </form>
         <p class="signup-main__form__paragraph signup-main__form__card">Already have an account? <a href="/login" class="signup-main__form__paragraph__link">Log in!</a></p>
     </main>
